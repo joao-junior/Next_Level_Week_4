a2c28719c765d3b3bd1e3acced41392fa3f49185
Joao Junior <joaojunior346@gmail.com>
2021-02-25 12:28:29 -0300

    conceito e aplicacao do typeOrm no NodeJs

---
+++ src/routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { SendEmailController } from './controllers/SendMailController';
+import { SurveysController } from './controllers/SurveysController';
+import { UserController } from './controllers/UserController';
+
+
+const router = Router();
+
+const userController = new UserController();
+const surveysController = new SurveysController();
+
+const sendEmailController = new SendEmailController();
+
+
+router.post("/users", userController.create);
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendEmailController.execute);
+
+export { router };
+
